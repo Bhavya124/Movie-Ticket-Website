831bda9ecf639ae1c536e8af6a8b4ddea48e2d5c
Bhavya 124 <bhavyasingla196@gmail.com>
2025-08-11 20:38:30 +0530

    4.00  hr completed

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
 
 function AddShows() {
+      const currency = import.meta.env.VIRE_CURRENCY
+      const [nowPlayingMovies , setNowPlayingMovies] = useState([]);
+      const [selectedMovie , setSelectedMovie] = useState(null);
+      const [dateTimeSelection , setDateTimeSelection] = useState({});
+      const [dateTimeInput , setDateTimeInput] = useState("");
+      const [showPrice , setShowPrice] = useState("");
+  
+
+        const fetchNowPlayingMovies = async() => {
+          setNowPlayingMovies(dummyShowsData)
+        };
+
+        useEffect(() => {
+          fetchNowPlayingMovies();
+        }, []);
+
   return (
     <div>
       
